dae95ffb95e0cfd90064a4e212d56e722f0a85cc
dineshjangid03 <dinesh.jangid323@gmail.com>
2023-02-24 15:45:26 +0530

    init

---
+++ js/script.js
@@ -0,0 +1,157 @@
+var tree={
+    root : {value : "Padma Family Kanod", parent : ""},
+    SevaJi : {value : "SevaJi", parent : "root"},
+
+    MadroopJi : {value : "MadroopJi", parent : "SevaJi"},
+
+    GourakhJi : {value : "GourakhJi", parent : "MadroopJi"},
+
+    ChandoJi : {value : "ChandoJi", parent : "GourakhJi"},
+    JinthoJi : {value : "JinthoJi", parent : "GourakhJi"},
+    JiyoJi : {value : "JiyoJi", parent : "GourakhJi"},
+
+    MaloJi : {value : "MaloJi", parent : "ChandoJi"},
+    ChenoJi : {value : "ChenoJi", parent : "ChandoJi"},
+    SongoJi : {value : "SongoJi", parent : "ChandoJi"},
+
+    HemoJi : {value : "HemoJi", parent : "JiyoJi"},
+    PrabhuJi : {value : "PrabhuJi", parent : "JiyoJi"},
+
+    MaghoJi : {value : "MaghoJi", parent : "MaloJi"},
+    KhinyoJi : {value : "KhinyoJi", parent : "MaloJi"},
+
+    VishanJi : {value : "VishanJi", parent : "ChenoJi"},
+    RatanJi : {value : "RatanJi", parent : "ChenoJi"},
+    JaloJi : {value : "JaloJi", parent : "ChenoJi"},
+    PonchoJi : {value : "PonchoJi", parent : "ChenoJi"},
+    DhanJi : {value : "DhanJi", parent : "ChenoJi"},
+    KaranJi : {value : "KaranJi", parent : "ChenoJi"},
+
+    KishturJi : {value : "KishturJi", parent : "SongoJi"},
+    BhagwanJi : {value : "BhagwanJi", parent : "SongoJi"},
+
+    VirmoJi : {value : "VirmoJi", parent : "HemoJi"},
+    KoshaloJi : {value : "KoshaloJi", parent : "HemoJi"},
+    VirdhoJi : {value : "VirdhoJi", parent : "HemoJi"},
+
+    MaghJi : {value : "MaghJi", parent : "PrabhuJi"},
+
+    DevJi : {value : "DevJi", parent : "DhanJi"},
+
+    LumbhJi : {value : "LumbhJi", parent : "KaranJi"},
+
+    PemJi : {value : "PemJi", parent : "BhagwanJi"},
+    BhagatJi : {value : "BhagatJi", parent : "BhagwanJi"},
+
+    JogJi : {value : "JogJi", parent : "VirmoJi"},
+    LalJi : {value : "LalJi", parent : "VirmoJi"},
+    ChimanJi : {value : "ChimanJi", parent : "VirmoJi"},
+    KonJi : {value : "KonJi", parent : "VirmoJi"},
+    PurJi : {value : "PurJi", parent : "VirmoJi"},
+    BhurJi : {value : "BhurJi", parent : "VirmoJi"},
+    KabirJi : {value : "KabirJi", parent : "VirmoJi"},
+
+    KhetoJi : {value : "KhetoJi", parent : "VirdhoJi"},
+
+    LabhuJi : {value : "LabhuJi", parent : "MaghJi"},
+
+    DeepJi : {value : "DeepJi", parent : "LumbhJi"},
+    HameerJi : {value : "HamirJi", parent : "LumbhJi"},
+    AaduJi : {value : "AidanJi", parent : "LumbhJi"},
+    ChutraRamJi : {value : "ChutraRamJi", parent : "LumbhJi"},
+
+    RoopJi : {value : "RoopJi", parent : "DevJi"},
+    AshuJi : {value : "AshuJi", parent : "DevJi"},
+    AmooJi : {value : "AmooJi", parent : "DevJi"},
+    ChokhJi : {value : "ChokhJi", parent : "DevJi"},
+    
+    BhanvarJi : {value : "BhanvarJi", parent : "PemJi"},
+    MangalJi : {value : "MangalJi", parent : "PemJi"},
+    AaduJiP : {value : "AaduJi", parent : "PemJi"},
+    RameshJi : {value : "RameshJi", parent : "PemJi"},
+
+    RamaJi : {value : "RamaJi", parent : "BhagatJi"},
+    MukJi : {value : "MukJi", parent : "BhagatJi"},
+    GovindJi : {value : "GovindJi", parent : "BhagatJi"},
+    MonJi : {value : "MonJi", parent : "BhagatJi"},
+    FatJi : {value : "FatJi", parent : "BhagatJi"},
+    NarpatJi : {value : "NarpatJi", parent : "BhagatJi"},
+
+    ChunJi : {value : "ChunJi", parent : "JogJi"},
+    ChouthJi : {value : "ChouthaRamJi", parent : "JogJi"},
+    
+    KhetoJiL : {value : "KhetaRamJi", parent : "LalJi"},
+    JagdishJi : {value : "JagdishJi", parent : "LalJi"},
+    
+    VedJi : {value : "VedJi", parent : "ChimanJi"},
+    ChetanJi : {value : "ChetanJi", parent : "ChimanJi"},
+    NarpatJi : {value : "NarpatJi", parent : "ChimanJi"},
+    RevantJi : {value : "RevantJi", parent : "ChimanJi"},
+
+    AambJi : {value : "AambJi", parent : "KonJi"},
+    RasingJi : {value : "RasingJi", parent : "KonJi"},
+    UdaRamJi : {value : "UdaRamJi", parent : "KonJi"},
+    
+    GomJi : {value : "GomJi", parent : "PurJi"},
+    AmedJi : {value : "AmedJi", parent : "PurJi"},
+    PrahladJi : {value : "PrahladJi", parent : "PurJi"},
+    BhanvarJi : {value : "BhanvarJi", parent : "PurJi"},
+
+    AmuJi : {value : "AmuJi", parent : "BhurJi"},
+    SagtaJi : {value : "SagtaJi", parent : "BhurJi"},
+    BabuJi : {value : "BabuJi", parent : "BhurJi"},
+    HukmaJi : {value : "HukmaJi", parent : "BhurJi"},
+
+    GomJik : {value : "GomJi", parent : "KabirJi"},
+    DurgJi : {value : "DurgJi", parent : "KabirJi"},
+
+    SukhJi : {value : "SukhJi", parent : "KhetoJi"},
+    
+    GomJil : {value : "GomJi", parent : "LabhuJi"},
+    RupJil : {value : "RupJi", parent : "LabhuJi"},
+    SogJi : {value : "SogJi", parent : "LabhuJi"},
+    UdJil : {value : "UdJi", parent : "LabhuJi"},
+    
+};
+  
+TreeData(tree, "#tree");
+  
+  
+function TreeData (data, select) {
+    
+    var main = document.querySelector(select);
+    var treecanvas = document.createElement('div');
+    treecanvas.className = 'tree';
+
+    var treeCode = buildTree(data, Object.keys(data)[0]);
+    treecanvas.innerHTML = treeCode;
+    main.appendChild(treecanvas);
+
+}
+
+
+function buildTree (obj, node) {
+
+    let x=(obj[node].value);
+    var treeString = "<li><a>" + x + "</a>";
+    var sons = [];
+
+    for (var i in obj) {
+        if (obj[i].parent == node)
+        sons.push(i);
+    }
+
+    if (sons.length > 0) {
+
+        treeString += "<ul>";
+
+        for (var i in sons) {
+        treeString += buildTree(obj, sons[i]);
+        }
+
+        treeString += "</ul>";
+
+    }
+    return treeString;
+
+}
